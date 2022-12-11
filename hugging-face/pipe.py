e6973bfe580091506feaa1172318d4dd0172ecfd
Dingkun Yang <dingkun.yang@duke.edu>
2022-12-11 17:59:09 -0500

    try huggingface

--- hugging-face/pipe.py
+++ hugging-face/pipe.py
@@ -1,6 +1,19 @@
 from transformers import pipeline
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
-classifier = pipeline("sentiment-analysis")
+# classifier = pipeline("sentiment-analysis")
+
+# res = classifier("I hate this movie")
+
+# print(res)
+
+model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
 
 res = classifier("I hate this movie")
 
