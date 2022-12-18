69d3a02d64c6cb9a2272b1ad01cc4ab482d0b0c4
Dingkun Yang <81750079+Yer1k@users.noreply.github.com>
2022-12-18 00:05:45 +0000

    preserve GPU branch code

--- 05_depreciated_files/generative_example.py
+++ 05_depreciated_files/generative_example.py
@@ -6,7 +6,7 @@ from nltk.corpus import stopwords
 import re
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import BernoulliNB
+from sklearn.naive_bayes import MultinomialNB
 
 
 def cleanhtml(sentence):
@@ -74,9 +74,9 @@ def main():
     vectorizer.fit(X)
     X = vectorizer.transform(X)
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.5, random_state=0, stratify=y
+        X, y, test_size=0.2, random_state=0, stratify=y
     )
-    model = BernoulliNB()
+    model = MultinomialNB()
     model.fit(X_train, y_train)
     print(model.score(X_train, y_train))
     print(model.score(X_test, y_test))
